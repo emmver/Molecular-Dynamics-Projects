0a2feeeda793a51d650a09bccbbea6e9e7933c08
emmver <74832386+emmver@users.noreply.github.com>
2022-01-19 11:25:10 +0100

    updated-script

--- Denpols/analysis.py
+++ Denpols/analysis.py
@@ -3,12 +3,7 @@ import pandas as pd # Library for data manipulation and analysis
 import os # Interfacing python with the operating system
 import matplotlib.pyplot as plt #Plotting library
 import seaborn as sns #Plotting library with some statistical tools 
-import nltk # Natural Language Processing toolkit, similar as textblob below, both have useful functions
-from textblob import TextBlob
-import string
-import time
-import re # Basically a string processing and manipulation library
-from datetime import datetime ## Manipulating datetime inputs
+
 from matplotlib.ticker import FuncFormatter
 import glob
 import matplotlib.pylab as pl
@@ -16,12 +11,12 @@ from ovito.io import import_file, export_file
 from numba import njit
 from tqdm import tqdm
 from hoomd_utils import * ## custom functions
-
+import sys
 ###### Data import ##########
 global to_store, nk, kgrid,knorm, nkbins,Sskarr,Ssktrans,Sskbb,dr,Lmax,nbins,c_r,cr_bb,Nth,btheta,jj,totrep,mygen,Lbackbone,mybox,nparticles
-os.chdir('/media/manosver/Elements/ManosVer/Denpols_extra/G3_N1000/RUN_1')
+os.chdir(sys.argv[1])
 print(os.getcwd())
-filename = './traj_1.gsd'
+filename = sys.argv[2]
 to_store = './results_' +filename[2:-4]
 if os.path.isdir(to_store)==False:
     os.mkdir(to_store)
@@ -30,7 +25,7 @@ data = pipeline.source.compute()
 ################################
 
 ### For analysis of structure factor ####
-_sk = ctypes.CDLL('/home/manosver/Documents/GitHub/emmver/Denpols/struc_fact/libsk.so')
+_sk = ctypes.CDLL('/mnt/c/Users/Toumba/Documents/GitHub/emmver/Denpols/struc_fact/libsk.so')
 _sk.calc_sk.argtypes = (ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p, ctypes.c_int, ctypes.c_int)
 
 nk = int(np.log(10./0.001)/np.log(1.05)); print ("How many k",nk)
@@ -47,8 +42,8 @@ cr_bb = np.zeros((nbins), dtype = float)
 
 jj = -1
 totrep = 0
-mygen = 3
-Lbackbone = 1000
+mygen = int(sys.argv[3])
+Lbackbone = int(sys.argv[4])
 mybox = data.cell[0,0]
 #########################################################################
 
@@ -75,13 +70,6 @@ plt.rcParams['ytick.major.pad']='8'
 #############################################
 
 
-
-
-
-
-
-
-os.chdir('/media/manosver/Elements/ManosVer/Denpols_extra/G3_N1000/RUN_1')
 print(os.getcwd())
 rgsq=[]
 lambda_1=[]
@@ -89,7 +77,7 @@ lambda_2=[]
 lambda_3=[]
 jj = -1
 totrep = 0
-for frame_index in tqdm(range(pipeline.source.num_frames-352)):
+for frame_index in tqdm(range(pipeline.source.num_frames),desc='G%d_N%d'%(mygen,Lbackbone)):
     #print("frame:",frame_index)
     data = pipeline.source.compute(frame_index)
     pos=data.particles.positions[:]
@@ -105,18 +93,7 @@ norm_sk(totrep,Sskarr,Sskbb,Ssktrans,knorm,nkbins,mygen,Lbackbone,pos[:][:,0].si
 
 gyr_tens=np.vstack([np.array(rgsq),np.array(lambda_1),np.array(lambda_2),np.array(lambda_3)])
 plot_shape(pipeline.source.num_frames,gyr_tens)
-
-
-os.chdir('/media/manosver/Elements/ManosVer/Denpols_extra/G3_N1000/RUN_1')
-file="./results_traj_1/G31000"
-data_w=np.genfromtxt(file)
-file="./results_traj_1/G31000_bb"
-data_bb=np.genfromtxt(file)
-plt.logscale(data_w[:,0],data_w[:,1],'r-')
-plt.logscale(data_bb[:,0],data_bb[:,1],'g-')
-plt.xlabel(r'$q[\sigma^-1]')
-plt.xlabel(r'$\frac{S(q)}{S(0)}')
-
+np.savetxt(tostore+'/gyration_tensor.txt',gyr_tens)
 
 
 
---
+++ Denpols/run_analysis.sh
@@ -0,0 +1,3 @@
+python3 analysis.py /mnt/d/University/Simulations/Dendronized_polymer/Analysis/HOOMD_DATA/G4_N1000/RUN_2/ traj.gsd 4 1000 & 
+python3 analysis.py /mnt/d/University/Simulations/Dendronized_polymer/Analysis/HOOMD_DATA/G4_N1000/RUN_2/ traj1.gsd  4 1000 & 
+python3 analysis.py /mnt/d/University/Simulations/Dendronized_polymer/Analysis/HOOMD_DATA/G5_N1000/RUN_2/ traj.gsd 5 1000
